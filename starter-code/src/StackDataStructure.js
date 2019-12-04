4002d626afab7900af8aa454aad166af283a2092
Juliane <juliane_ch.arruda@live.com>
2019-12-04 11:30:38 -0300

    done

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,26 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor(MAX_SIZE = 10) {
+    this.stackControl = [];
+    this.MAX_SIZE = MAX_SIZE;
+    this.size = 0;
+  }
+  
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  canPush() {
+    return this.size < this.MAX_SIZE;
+  }
+
+  push(item) {
+    if (this.canPush()) {
+      this.stackControl[this.size] = item;
+      
+    }
+
+    return this.size > this.MAX_SIZE;
+  }
+
+  pop() {}
 }
